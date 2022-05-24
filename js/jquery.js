1579c2423d003a04ecad1365667b5f413bfc6fac
SHINNOSUKE-UENO <s_ueshi@outlook.jp>
2022-05-24 23:16:25 +0900

    second-index

--- js/jquery.js
+++ js/jquery.js
@@ -4,18 +4,40 @@ document.querySelector(`.rotate`).animate(
         { transform: 'rotate(360deg)' }
     ],
     {
-        duration: 5000,
+        duration: 8000,
         easing: 'linear',
         iterations: Infinity
     }
 );
-document.querySelector(`.horizontal-move`).animate(
+
+const first = document.getElementById(`first_slide-up`);
+function second() {
+    document.querySelector(`.horizontal-move`).animate(
     [
         { transform: 'translateX(0)' },
-        { transform: 'translate(100vw)' }
+        { transform: 'translate(140vw)' }
     ],
     {
         duration: 10000,
         iterations: Infinity
     }
 );
+}
+
+function asynchronyFunction() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            first.style.display = 'block';
+            resolve();
+        },800);
+    })
+    .then(() => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                second();
+                resolve();
+            },1500);
+        })
+    });
+}
+asynchronyFunction();
